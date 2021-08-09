717f6cb673b584461311414b55f66faea7cb728d
Vinicius Cainelli <vinicius.cainelli@takeaway.com>
2021-08-09 12:39:04 +0200

    add tailwindcss

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should renders the App correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const el = screen.getByText(/Cypress Component Testing with Tailwind CSS/i);
+  expect(el).toBeInTheDocument();
 });
